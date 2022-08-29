2840dfc807b04c4d527af5ea801eeab52fdd8197
MiltonBlack <arziblack2@gmail.com>
2022-08-29 07:26:15 +0100

    Added custom colors to tailwind.config.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="">
-      <h1 className=' text-lg bg-blue-600'>hello</h1>
+      <h1 className=' text-lg bg-blue-600'>hello world</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      'body':'#17171f',
+      'selected-text': '#a3a3ff',
+      'theme':'#3f3fff',
+      'nav':'#404053',
+      'secondary':'#9191a4',
+      'badge':'#3f3f51',
+      'input-border':'#565666',
+      'input':'#2a2a35'
+    },
     extend: {},
   },
   plugins: [],
